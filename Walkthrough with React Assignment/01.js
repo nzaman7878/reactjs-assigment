a9c6a50c80ec87192a4e774b4b36e429413cb155
NURUZZAMAN <117540775+nzaman7878@users.noreply.github.com>
2023-08-29 23:47:46 +0530

    Add files via upload

---
+++ Walkthrough with React Assignment/01.js
@@ -0,0 +1,16 @@
+/* Create a component named "Person" that takes two 
+props - "name" and "age". The component should display 
+the person's name and age in a paragraph element.
+ */
+
+function Person(props) {
+  return (
+    <p>
+      {props.name} is {props.age} years old.
+    </p>
+  );
+}
+
+// Usage
+<Person name="John" age="30" />
+
