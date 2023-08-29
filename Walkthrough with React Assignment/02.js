a2bf6bc33c063543eb4385397ceb93078e41053d
NURUZZAMAN <117540775+nzaman7878@users.noreply.github.com>
2023-08-29 23:48:06 +0530

    Add files via upload

---
+++ Walkthrough with React Assignment/02.js
@@ -0,0 +1,17 @@
+/* Create a component named "Button" that takes two props - "text" 
+and "onClick". The component should display a button element with the
+ given text and call the onClick function when clicked.
+ */
+
+ function Button(props) {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  );
+}
+
+// Usage
+<Button text="Click me!" onClick={() => console.log("Button clicked")} />
+
+
